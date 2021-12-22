dc4f4a5b45e0ad87620c61ed470ec94bb24f40da
笑笑 <xxred0324@qq.com>
2021-12-22 14:17:21 +0800

    store使用createStore导出

--- src/src/store/index.ts
+++ src/src/store/index.ts
@@ -1,15 +1,37 @@
 import route from './modules/route'
 import user from './modules/user'
 import entity from './modules/entity'
-import app from './modules/app'
+import moduleApp from './modules/app'
 import getters from './getters'
+import { createStore as create, Store, StoreOptions } from 'vuex'
+import { App } from 'vue'
 
-export default {
+interface S {
+  app: typeof moduleApp.state
+  entity: typeof entity.state
+  route: typeof route.state
+  user: typeof user.state
+}
+
+export const storeOptions: StoreOptions<S> = {
   modules: {
-    route,
-    user,
+    moduleApp,
     entity,
-    app
+    route,
+    user
   },
   getters
+}
+
+export const createStore = (
+  app: App<Element>,
+  configure: ((options: StoreOptions<S>) => void) | undefined | null = null
+): Store<S> => {
+  if (configure) {
+    configure(storeOptions)
+  }
+  const store = create(storeOptions)
+  app.use(store)
+
+  return store
 }
