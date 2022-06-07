34dbaf22aed5699d92bc8fc752282125dfbfa313
笑笑 <xxred0324@qq.com>
2022-06-07 12:36:07 +0800

    更新cdn地址

--- demo/vue.config.js
+++ demo/vue.config.js
@@ -22,13 +22,14 @@ module.exports = {
           args[0].cdn = {
             js: [
               // 'https://cdn.jsdelivr.net/npm/vue',
-              'https://unpkg.com/vue-router/dist/vue-router.js',
-              'https://cdn.bootcss.com/vuex/3.6.2/vuex.min.js',
+              'https://cdn.bootcdn.net/ajax/libs/vue/3.2.20/vue.min.js',
+              'https://cdn.bootcdn.net/ajax/libs/vue-router/4.0.12/vue-router.cjs.min.js',
+              'https://cdn.bootcdn.net/ajax/libs/vuex/4.0.2/vuex.cjs.min.js',
               'https://cdn.bootcdn.net/ajax/libs/axios/0.21.1/axios.min.js',
-              'https://unpkg.com/element-ui@2.15.0/lib/index.js'
+              'https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.8/index.min.js'
             ],
             css: [
-              'https://unpkg.com/element-ui@2.15.0/lib/theme-chalk/index.css'
+              'https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.8/theme-chalk/index.min.css'
             ]
           }
           console.log(args[0])
