9bb4b1e4e2f9b2dadb404ed04f40e49b2b994b61
笑笑 <xxred0324@qq.com>
2023-03-03 17:53:14 +0800

    增加用户列配置覆盖

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const isProduction = process.env.NODE_ENV === 'production'
-console.log(isProduction)
+const isProduction = process.env.NODE_ENV === 'production';
+console.log(isProduction);
 
 module.exports = {
   chainWebpack: (config) => {},
@@ -10,38 +10,38 @@ module.exports = {
           root: 'Vue', // 指向全局变量
           commonjs: 'vue',
           commonjs2: 'vue',
-          amd: 'vue'
+          amd: 'vue',
         },
         vuex: {
           root: 'Vuex', // 指向全局变量
           commonjs: 'vuex',
           commonjs2: 'vuex',
-          amd: 'vuex'
+          amd: 'vuex',
         },
 
         'vue-router': {
           root: 'VueRouter', // 指向全局变量
           commonjs: 'vue-router',
           commonjs2: 'vue-router',
-          amd: 'vue-router'
+          amd: 'vue-router',
         },
         axios: 'axios',
         'element-plus': {
           root: 'ElementPlus', // 指向全局变量
           commonjs: 'element-plus',
           commonjs2: 'element-plus',
-          amd: 'element-plus'
+          amd: 'element-plus',
         },
         '@element-plus/icons-vue': {
           root: 'ElementPlusIcons', // 指向全局变量
           commonjs: '@element-plus/icons-vue',
           commonjs2: '@element-plus/icons-vue',
-          amd: '@element-plus/icons-vue'
-        }
-      }
+          amd: '@element-plus/icons-vue',
+        },
+      };
     }
 
     // 源码映射，用于调试
-    config.devtool = 'source-map'
-  }
-}
+    config.devtool = 'source-map';
+  },
+};
