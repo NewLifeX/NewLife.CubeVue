5f7797f0fb94cee71eb72a21f5201dc4ddcdbbd2
笑笑 <xxred0324@qq.com>
2023-03-13 11:23:01 +0800

    服务器信息页面重命名

--- src/views/Admin/Index/Main/list.vue
+++ src/views/Admin/Index/Main/list.vue
@@ -6,74 +6,56 @@
       >
         <thead>
           <tr>
-            <th colspan="4">
-              服务器信息
-            </th>
+            <th colspan="4">服务器信息</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td class="name">
-              应用系统：
-            </td>
+            <td class="name">应用系统：</td>
             <td class="value">
               <!-- @if (this.has((PermissionFlags)16)) -->
 
-              <a style="cursor: pointer;" @click="restart">重启应用系统</a>
+              <a style="cursor: pointer" @click="restart">重启应用系统</a>
 
               &nbsp;&nbsp;&nbsp;&nbsp;{{ model.rawUrl }}
             </td>
-            <td class="name">
-              目录：
-            </td>
+            <td class="name">目录：</td>
             <td class="value">
               {{ model.contentRootPath }}
             </td>
           </tr>
           <tr>
-            <td class="name">
-              域名地址：
-            </td>
+            <td class="name">域名地址：</td>
             <td class="value">
               <span title="主机">{{ model.host }}</span
               >，
               <span title="本地">{{ model.localHost }}</span>
               &nbsp;<span title="远程">{{ model.remoteHost }}</span>
             </td>
-            <td class="name">
-              应用程序：
-            </td>
+            <td class="name">应用程序：</td>
             <td class="value">
               <span :title="model.commandLine">{{ model.processName }}</span>
             </td>
           </tr>
           <tr>
-            <td class="name">
-              应用域：
-            </td>
+            <td class="name">应用域：</td>
             <td class="value">
               {{ model.curDomainFriendlyName }}
               <!-- <a href="@Url.action("Main", new{ id = "Assembly" })" target="_blank" title="点击打开进程程序集列表">程序集列表</a>
-                    <a href="@Url.action("Main", new{ id = "ProcessModules" })" target="_blank" title="点击打开进程模块列表">模块列表</a>
-                    <a href="@Url.action("Main", new{ id = "ServerVar" })" target="_blank" title="点击打开服务器变量列表">服务器变量列表</a> -->
+                      <a href="@Url.action("Main", new{ id = "ProcessModules" })" target="_blank" title="点击打开进程模块列表">模块列表</a>
+                      <a href="@Url.action("Main", new{ id = "ServerVar" })" target="_blank" title="点击打开服务器变量列表">服务器变量列表</a> -->
             </td>
-            <td class="name">
-              .net 版本：
-            </td>
+            <td class="name">.net 版本：</td>
             <td class="value">
               {{ model.envVersion }} &nbsp;{{ model.frameworkName }}
             </td>
           </tr>
           <tr>
-            <td class="name">
-              操作系统：
-            </td>
+            <td class="name">操作系统：</td>
             <td class="value" :title="model.guid">
               {{ model.oSName }} {{ model.oSVersion }}
             </td>
-            <td class="name">
-              机器用户：
-            </td>
+            <td class="name">机器用户：</td>
             <td class="value" :title="model.uuid">
               <span v-if="model.product !== undefined"
                 >{{ model.product }}，</span
@@ -82,9 +64,7 @@
             </td>
           </tr>
           <tr>
-            <td class="name">
-              处理器：
-            </td>
+            <td class="name">处理器：</td>
             <td class="value" :title="model.cpuID">
               {{ model.processor }}，
               {{ model.processorCount }}
@@ -93,9 +73,7 @@
                 >，{{ model.temperature }} ℃</span
               >
             </td>
-            <td class="name">
-              时间：
-            </td>
+            <td class="name">时间：</td>
 
             <td
               class="value"
@@ -105,9 +83,7 @@
             </td>
           </tr>
           <tr>
-            <td class="name">
-              内存：
-            </td>
+            <td class="name">内存：</td>
             <td class="value">
               物理：{{ model.availableMemory }}M / {{ model.memory }}M，
               工作/提交: {{ model.workingSet64 }}M/{{
@@ -115,17 +91,13 @@
               }}M GC: {{ model.totalMemory }}M
               <a @click="memoryFree" title="点击释放进程内存">释放内存</a>
             </td>
-            <td class="name">
-              进程时间：
-            </td>
+            <td class="name">进程时间：</td>
             <td class="value">
               <!-- @process.totalProcessorTime.totalSeconds.toString("N2")秒 启动于 @process.startTime.toLocalTime().toFullString() -->
             </td>
           </tr>
           <tr>
-            <td class="name">
-              Session：
-            </td>
+            <td class="name">Session：</td>
             <td class="value">
               <!-- @httpContext.session.keys.count() 个 -->
               <a
@@ -161,16 +133,16 @@
         </thead>
         <tbody>
           <!-- @foreach (AssemblyX item in ViewBag.myAsms)
-            {
-                <tr>
-                    <td>@item.name</td>
-                    <td>@item.title</td>
-                    <td>@item.fileVersion</td>
-                    <td>@item.version</td>
-                    <td>@(item.compile.year <= 2000 ? "" : item.compile.toFullString())</td>
-                    <td>@item.description</td>
-                </tr>
-            } -->
+              {
+                  <tr>
+                      <td>@item.name</td>
+                      <td>@item.title</td>
+                      <td>@item.fileVersion</td>
+                      <td>@item.version</td>
+                      <td>@(item.compile.year <= 2000 ? "" : item.compile.toFullString())</td>
+                      <td>@item.description</td>
+                  </tr>
+              } -->
         </tbody>
       </table>
     </div>
@@ -211,7 +183,7 @@ export default {
         privateMemorySize64: 'PrivateMemorySize64',
         totalMemory: 'TotalMemory',
       },
-    }
+    };
   },
   methods: {
     restart() {
@@ -221,19 +193,19 @@ export default {
         {
           confirmButtonText: 'confirm',
           callback: (action) => {
-            console.log(action)
+            console.log(action);
           },
-        }
-      )
+        },
+      );
     },
     memoryFree() {
-      console.log('memoryFree')
+      console.log('memoryFree');
     },
     main(id) {
-      console.log(id)
+      console.log(id);
     },
   },
-}
+};
 </script>
 
 <style></style>
