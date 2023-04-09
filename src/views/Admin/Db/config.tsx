ee87a5861f0544c66156fdbf6a0c717fb215d3b0
笑笑 <xxred0324@qq.com>
2023-04-09 13:22:14 +0800

    增加DB列表字段配置

---
+++ src/views/Admin/Db/config.tsx
@@ -0,0 +1,29 @@
+export const tableColumnConfig = [
+  {
+    name: 'name',
+    displayName: '名称',
+    showInList: true,
+  },
+  {
+    name: 'type',
+    displayName: '类型',
+    itemType: null,
+    showInList: true,
+  },
+  {
+    name: 'connStr',
+    displayName: '连接字符串',
+    showInList: true,
+  },
+  {
+    name: 'version',
+    displayName: '版本',
+    itemType: null,
+    showInList: true,
+  },
+  {
+    name: 'backups',
+    displayName: '备份',
+    showInList: true,
+  },
+];
